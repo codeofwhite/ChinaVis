3bce1159497d06ce33a9002f6bb059ac1a4b01ae
qiaolann <1662652109@qq.com>
2025-06-03 02:37:23 +0800

    更新了长城和北海公园的二级页面和图片

--- china_vis2025/src/App.vue
+++ china_vis2025/src/App.vue
@@ -1,50 +1,10 @@
+<!-- App.vue -->
 <template>
-  <div class="app-container">
-    <h1>故宫生命历程可视化</h1>
-    <TimeLine :events="forbiddenCity.timeline" @show-network="openModal" />
-
-    <!-- 网络图弹窗 -->
-    <el-dialog
-      v-model="dialogVisible"
-      :title="
-        selectedEvent ? `${selectedEvent.year}年: ${selectedEvent.event}` : ''
-      "
-      width="80%"
-      top="5vh"
-    >
-      <LandmarkNetwork
-        v-if="dialogVisible"
-        :landmark="forbiddenCity"
-        :event="selectedEvent"
-      />
-    </el-dialog>
-  </div>
+  <router-view /> /landmark/:landmarkId <!-- 这里是路由出口，会显示当前路由对应的组件 -->
 </template>
 
-<script setup>
-import { ref } from "vue";
-import TimeLine from "./components/TimeLine.vue";
-import LandmarkNetwork from "./components/LandmarkNetwork.vue";
-import forbiddenCityData from "./assets/forbidden-city.json";
-
-const forbiddenCity = ref(forbiddenCityData);
-const dialogVisible = ref(false);
-const selectedEvent = ref(null);
-
-const openModal = (event) => {
-  selectedEvent.value = event;
-  dialogVisible.value = true;
-};
+<script>
+export default {
+  name: 'App'
+}
 </script>
-
-<style scoped>
-.app-container {
-  max-width: 1200px;
-  margin: 0 auto;
-  padding: 20px;
-}
-h1 {
-  text-align: center;
-  color: #2c3e50;
-}
-</style>
